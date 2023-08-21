4028eba69c6b06e1d02a15019ab88b0711a5d16c
Hamad Bakeel <hamadbakeel@gmail.com>
2023-08-21 10:58:15 +0300

    feat: changed the weekdays to arabic

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ const deleteEventModal = document.getElementById("deleteEventModal");
 const backDrop = document.getElementById("modalBackDrop");
 const eventTitleInput = document.getElementById("eventTitleInput");
 const weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
+  "السبت",
+  "الأحد",
+  "الاثنين",
+  "الثلاثاء",
+  "الأربعاء",
+  "الخميس",
+  "الجمعة",
 ];
 
 function openModal(date) {
@@ -33,87 +33,87 @@ function openModal(date) {
 
   backDrop.style.display = "block";
 }
-output(getHijriMonthInfo(new Date(Date.now()))); // today's date
-output(getHijriMonthInfo("2022-04-22"));         // 22 April 2022
-//==================
-function output([gDate, iDay, iMonth, iYear, iMonthTotalDays, gStart, gEnd]) {
-
-console.log(`
-Gregorian Date            : ${gDate}
-Hijri Day                 : ${iDay}
-Hijri Month               : ${iMonth}
-Hijri Year                : ${iYear}
-Total Days in Hijri Month : ${iMonthTotalDays}
-Hijri Month Starts on     : ${gStart}
-Hijri Month Ends on       : ${gEnd}
-`
-);
-}
 function load() {
-    const dt = new Date();
-  
-    if (nav !== 0) {
-      dt.setMonth(new Date().getMonth() + nav);
-    }
-  
-    const day = dt.getDate();
-    const month = dt.getMonth();
-    const year = dt.getFullYear();
-  
-    const firstDayOfMonth = new Date(year, month, 1);
-    const daysInMonth = new Date(year, month + 1, 0).getDate();
-  
-    const dateString = firstDayOfMonth.toLocaleDateString("en-us", {
-      weekday: "long",
-      year: "numeric",
-      month: "numeric",
-      day: "numeric",
-    });
-    const paddingDays = weekdays.indexOf(dateString.split(", ")[0]);
-  
-    document.getElementById("monthDisplay").innerText = `${dt.toLocaleDateString(
-      "en-us",
-      { month: "long" }
-    )} ${year}`;
-  
-    calendar.innerHTML = "";
-  
-    for (let i = 1; i <= paddingDays + daysInMonth; i++) {
-      const daySquare = document.createElement("div");
-      daySquare.classList.add("day");
-  
-      const dayString = `${month + 1}/${i - paddingDays}/${year}`;
-  
-      if (i > paddingDays) {
-        daySquare.innerText = i - paddingDays;
-        const hijriDay = document.createElement('div');
-        const iDay = new Intl.DateTimeFormat('ar-TN-u-ca-islamic', {day: 'numeric'}).format(new Date(year, month, i - paddingDays));
-        const iMonth = new Intl.DateTimeFormat('ar-TN-u-ca-islamic', { month: 'long'}).format(new Date(year, month, i - paddingDays));
-        const hijriDate = new Intl.DateTimeFormat('ar-TN-u-ca-islamic', {day: 'numeric', month: 'long', weekday: 'long', year: 'numeric'}).format(new Date(year, month, i - paddingDays));
-        hijriDay.textContent = iDay == 1 ? iMonth : iDay;
-        daySquare.appendChild(hijriDay);
-  
-        const eventForDay = events.find((e) => e.date === dayString);
-  
-        if (i - paddingDays === day && nav === 0) {
-          daySquare.id = "currentDay";
-        }
-  
-        if (eventForDay) {
-          const eventDiv = document.createElement("div");
-          eventDiv.classList.add("event");
-          eventDiv.innerText = eventForDay.title;
-          daySquare.appendChild(eventDiv);
-        }
-  
-        daySquare.addEventListener("click", () => openModal(dayString));
-      } else {
-        daySquare.classList.add("padding");
-      }
-  
-      calendar.appendChild(daySquare);
-    }
+  const dt = new Date();
+
+  if (nav !== 0) {
+    dt.setMonth(new Date().getMonth() + nav);
   }
+
+  const day = dt.getDate();
+  const month = dt.getMonth();
+  const year = dt.getFullYear();
+
+  const firstDayOfMonth = new Date(year, month, 1);
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+  const dateString = firstDayOfMonth.toLocaleDateString("ar-us", {
+    weekday: "long",
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  });
+  console.log(dateString);
+  console.log(dateString.split("،")[0]);
+  const paddingDays = weekdays.indexOf(dateString.split("،")[0]);
+  console.log(paddingDays);
+
+  document.getElementById("monthDisplay").innerText = `${dt.toLocaleDateString(
+    "ar-us",
+    { month: "long" }
+  )} ${year}`;
+
+  calendar.innerHTML = "";
+
+  for (let i = 1; i <= paddingDays + daysInMonth; i++) {
+    const daySquare = document.createElement("div");
+    daySquare.classList.add("day");
+
+    const dayString = `${month + 1}/${i - paddingDays}/${year}`;
+
+    if (i > paddingDays) {
+      daySquare.innerText = i - paddingDays;
+      const hijriDay = document.createElement("div");
+      const iDay = new Intl.DateTimeFormat("ar-TN-u-ca-islamic", {
+        day: "numeric",
+      }).format(new Date(year, month, i - paddingDays));
+      const iMonth = new Intl.DateTimeFormat("ar-TN-u-ca-islamic", {
+        month: "long",
+      }).format(new Date(year, month, i - paddingDays));
+      const iWeekday = new Intl.DateTimeFormat("ar-TN-u-ca-islamic", {
+        weekday: "long",
+      }).format(new Date(year, month, i - paddingDays));
+    //   console.log(iWeekday);
+      const hijriDate = new Intl.DateTimeFormat("ar-TN-u-ca-islamic", {
+        day: "numeric",
+        month: "long",
+        weekday: "long",
+        year: "numeric",
+      }).format(new Date(year, month, i - paddingDays));
+      hijriDay.textContent = iDay == 1 ? iMonth : iDay;
+      daySquare.appendChild(hijriDay);
+
+      const eventForDay = events.find((e) => e.date === dayString);
+
+      if (i - paddingDays === day && nav === 0) {
+        daySquare.id = "currentDay";
+      }
+
+      if (eventForDay) {
+        const eventDiv = document.createElement("div");
+        eventDiv.classList.add("event");
+        eventDiv.innerText = eventForDay.title;
+        daySquare.appendChild(eventDiv);
+      }
+
+      daySquare.addEventListener("click", () => openModal(dayString));
+    } else {
+      daySquare.classList.add("padding");
+    }
+
+    calendar.appendChild(daySquare);
+  }
+}
 
 function closeModal() {
   eventTitleInput.classList.remove("error");
@@ -166,38 +166,6 @@ function initButtons() {
   document.getElementById("closeButton").addEventListener("click", closeModal);
 }
 
-function getHijriMonthInfo(date) {
-  let startDate = new Date(date),
-    c = "ar-u-ca-islamic-umalqura-nu-latn", // use 'islamic-umalqura' calendar for the islamic date
-    d = startDate,
-    gDays = 0,
-    iMonthTotalDays = 0,
-    i_Day,
-    n = "numeric",
-    iDay = new Intl.DateTimeFormat(c, { day: n }).format(startDate),
-    iMonth = new Intl.DateTimeFormat(c, { month: "long" }).format(startDate),
-    iYear = new Intl.DateTimeFormat(c, { year: n })
-      .format(startDate)
-      .split(" ")[0];
-  for (let i = 0; i < 32; i++) {
-    i_Day = new Intl.DateTimeFormat(c, { day: n }).format(d); // Hijri day
-    if (+i_Day > iMonthTotalDays) (iMonthTotalDays = i_Day), gDays++;
-    else break;
-    d = new Date(d.setDate(d.getDate() + 1)); // next Gregorian day
-  }
-  let gEndT = new Date(startDate.setDate(startDate.getDate() + gDays - 2)),
-    gEnd = new Date(gEndT),
-    gStart = new Date(gEndT.setDate(gEndT.getDate() - iMonthTotalDays + 1));
-  return [
-    new Date(date).toISOString().split("T")[0],
-    +iDay,
-    iMonth,
-    +iYear,
-    iMonthTotalDays,
-    gStart,
-    gEnd,
-  ];
-}
 
 initButtons();
 load();
